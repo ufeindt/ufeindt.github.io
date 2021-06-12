a2f6aaebae110489f09600878271c47cf2eb09a5
Ulrich Feindt <ufeindt@gmail.com>
2021-06-12 17:10:49 +0200

    WIP

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,5 +32,19 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "myMarkdown",
+        path: `${__dirname}/content`,
+      },
+    },
+    `gatsby-transformer-remark`,
   ],
 }
