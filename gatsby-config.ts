47faef7bc4b3ac977256a843a8e365a945905233
Ulrich Feindt <ufeindt@gmail.com>
2024-01-14 16:47:54 +0100

    tmp

---
+++ gatsby-config.ts
@@ -0,0 +1,51 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Ulrich Feindt`,
+    siteUrl: `https://feindt.dev`
+
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/icon.png"
+      }
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
+    },
+    `gatsby-transformer-yaml`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./content/`,
+      },
+    },
+  ]
+};
+
+export default config;
--- src/utils/typography.js
+++
@@ -1,8 +0,0 @@
-import Typography from "typography"
-import irvingTheme from "typography-theme-irving"
-
-const typography = new Typography(irvingTheme)
-
-// Export helper functions
-export const { scale, rhythm, options } = typography
-export default typography
